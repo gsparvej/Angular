63a4e62cb8505498170449a9148ad1f3ab6a6a79
gsparvej <parvejmd4469@gmail.com>
2025-07-01 12:49:45 +0600

    addedAuth

---
+++ myprojectdemo/src/app/service/auth-service.ts
@@ -0,0 +1,94 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { UserModel } from '../../model/user.model';
+import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
+import { AuthResponse } from '../../model/authResponse.model';
+import { error } from 'console';
+import { isPlatformBrowser } from '@angular/common';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+ baseUrl: string ="http://localhost:3000/user";
+  private currentUserSubject: BehaviorSubject<UserModel | null>;
+  public currentUser$: Observable<UserModel | null>;
+
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object  // Injecting PLATFORM_ID to check if it's browser
+  ) {
+    const storedUser = this.isBrowser() ? JSON.parse(localStorage.getItem('currentUser') || 'null') : null;
+    this.currentUserSubject = new BehaviorSubject<UserModel | null>(storedUser);
+    this.currentUser$ = this.currentUserSubject.asObservable();
+  }
+
+registration(user: UserModel): Observable<AuthResponse> {
+
+return this.http.post<UserModel>(this.baseUrl,user).pipe(
+  map((newUser: UserModel) => {
+
+    // create token by username and password
+
+    const token = btoa(`${newUser.email}${newUser.password}`);
+    return { token, user: newUser} as AuthResponse;
+
+  }),
+  catchError(error => {
+console.error('Registration Error',error);
+throw error;
+  })
+);
+
+}
+
+private isBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
+
+login(credentials: { email: string; password: string }): Observable<AuthResponse> {
+    let params = new HttpParams().append('email', credentials.email);
+
+    return this.http.get<UserModel[]>(`${this.baseUrl}`, { params }).pipe(
+      map(users => {
+        if (users.length > 0) {
+          const user = users[0];
+          if (user.password === credentials.password) {
+            const token = btoa(`${user.email}:${user.password}`);
+            this.storeToken(token);
+            this.setCurrentUser(user);
+            return { token, user } as AuthResponse;
+          } else {
+            throw new Error('Invalid password');
+          }
+        } else {
+          throw new Error('User not found');
+        }
+      }),
+      catchError(error => {
+        console.error('Login error:', error);
+        throw error;
+      })
+    );
+  }
+
+  storeToken(token: string): void {
+    if (this.isBrowser()) {
+      localStorage.setItem('token', token);
+    }
+  }
+
+
+
+   private setCurrentUser(user: UserModel): void {
+    if (this.isBrowser()) {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+    }
+    this.currentUserSubject.next(user);
+  }
+
+
+
+}
